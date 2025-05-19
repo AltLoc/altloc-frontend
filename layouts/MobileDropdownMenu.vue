ee8eddd10839b0eca7e9d926d4d9a5f323344b30
progof <34421475+progof@users.noreply.github.com>
2025-05-19 19:31:42 +0200

    upd

--- layouts/MobileDropdownMenu.vue
+++ layouts/MobileDropdownMenu.vue
@@ -8,27 +8,22 @@ import {
   DropdownMenuTrigger,
   DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu";
-import GameControllerIcon from "@/assets/icons/game-controller.svg?component";
-import CommentIcon from "@/assets/icons/comment.svg?component";
 import SettingsIcon from "@/assets/icons/settings.svg?component";
 import LogoutIcon from "@/assets/icons/logout.svg?component";
 import LoaderIcon from "@/assets/icons/loader.svg?component";
-import PeopleIcon from "@/assets/icons/people.svg?component";
 import AstronautHelmetIcon from "@/assets/icons/astronaut-helmet.svg?component";
-import GridOnSharpIcon from "@/assets/icons/grid-on-sharp.svg?component";
-import { getMeQueryOptions } from "@/features/user/service/user.client";
-import type { User } from "@/models";
 import { useQueryClient } from "@tanstack/vue-query";
-import { getCDNImageURL } from "@/utils/images";
 import { useRouter } from "vue-router";
 import { useLogoutMutation } from "@/features/auth/service/authPassword";
 import { useI18n } from "vue-i18n";
+import DotsHorizontalIcon from "@/assets/icons/dots-horizontal.svg?component";
+import QuestionMartIcon from "@/assets/icons/question-mark.svg?component";
 
 const { t } = useI18n();
-const props = defineProps<{ user: User }>();
+// const props = defineProps<{ user: User }>();
 
 const queryClient = useQueryClient();
-queryClient.setQueryData(getMeQueryOptions.queryKey, props.user);
+// queryClient.setQueryData(getMeQueryOptions.queryKey, props.user);
 
 const router = useRouter();
 const { mutate: logout, isPending } = useLogoutMutation();
@@ -49,30 +44,21 @@ const isActive = (path: string) => {
 
 <template>
   <DropdownMenu :modal="false">
-    <DropdownMenuTrigger class="flex items-center gap-2.5">
-      <span class="text-sm font-semibold text-zinc-100">
-        {{ user.username }}
-      </span>
-      <img
-        alt="User avatar"
-        :src="
-          user?.avatarKey
-            ? getCDNImageURL(user.avatarKey)
-            : '/images/placeholder_image.webp'
-        "
-        class="size-10 shrink-0 rounded-full border border-black/10 transition-colors group-hover:border-black/20"
-      />
+    <DropdownMenuTrigger
+      class="p-1 hover:bg-blue-700 rounded-xl transition-colors justify-center data-[state=open]:bg-blue-700"
+      aria-label="Matrix options"
+    >
+      <DotsHorizontalIcon class="size-6 stroke-[1.5] text-white" />
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end" class="min-w-48">
-      <DropdownMenuLabel>
+      <!-- <DropdownMenuLabel>
         <div class="flex flex-col">
           <span class="text-zinc-700">{{ user.username }}</span>
           <span class="text-xs font-normal text-zinc-600">
             {{ user.email }}
           </span>
         </div>
-      </DropdownMenuLabel>
-      <DropdownMenuSeparator />
+      </DropdownMenuLabel> -->
       <DropdownMenuGroup>
         <DropdownMenuItem
           as="a"
@@ -80,46 +66,28 @@ const isActive = (path: string) => {
           :data-active="isActive('/user/profile')"
           class="group flex items-center gap-2 rounded-md px-3 py-2 hover:bg-zinc-100 data-[active]:hover:bg-indigo-50"
         >
+          <QuestionMartIcon class="mr-2 size-4 stroke-[1.5] text-zinc-800" />
+          <span class="text-zinc-800">
+            {{ t("app.cabinet.nav.help") }}
+          </span>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem
+          as="a"
+          href="#"
+          :data-active="isActive('#')"
+          class="group flex items-center gap-2 rounded-md px-3 py-2 hover:bg-zinc-100 data-[active]:hover:bg-indigo-50"
+        >
           <AstronautHelmetIcon class="mr-2 size-4 stroke-[1.5] text-zinc-800" />
           <span class="text-zinc-800">
             {{ t("app.cabinet.nav.profile") }}
           </span>
         </DropdownMenuItem>
-        <DropdownMenuItem
-          as="a"
-          href="/user/quests"
-          :data-active="isActive('/user/quests')"
-          class="group flex items-center gap-2 rounded-md px-3 py-2 hover:bg-zinc-100 data-[active]:hover:bg-indigo-50"
-        >
-          <GameControllerIcon class="mr-2 size-4 stroke-[1.5] text-zinc-800" />
-          <span class="text-zinc-800">
-            {{ t("app.cabinet.nav.quests") }}
-          </span>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          as="a"
-          href="/user/daily-comment"
-          :data-active="isActive('/user/daily-comment')"
-          class="group flex items-center gap-2 rounded-md px-3 py-2 hover:bg-zinc-100 data-[active]:hover:bg-indigo-50"
-        >
-          <CommentIcon class="mr-2 size-4 stroke-[1.5] text-zinc-800" />
-          <span class="text-zinc-800">
-            {{ t("app.cabinet.nav.dailyComment") }}
-          </span>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          as="a"
-          href="/user/matrix"
-          :data-active="isActive('/user/matrix')"
-          class="group flex items-center gap-2 rounded-md px-3 py-2 hover:bg-zinc-100 data-[active]:hover:bg-indigo-50"
-        >
-          <GridOnSharpIcon class="mr-2 size-4 stroke-[1.5] text-zinc-800" />
-          <span class="text-zinc-800">
-            {{ t("app.cabinet.nav.matrixAndDomains") }}
-          </span>
-        </DropdownMenuItem>
       </DropdownMenuGroup>
-      <template v-if="user.isAdmin">
+
+      <!-- <template v-if="user.isAdmin">
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem
@@ -132,7 +100,7 @@ const isActive = (path: string) => {
             <span class="text-zinc-800">Users</span>
           </DropdownMenuItem>
         </DropdownMenuGroup>
-      </template>
+      </template> -->
       <DropdownMenuSeparator />
       <DropdownMenuGroup>
         <DropdownMenuItem
--- layouts/MobileIslandMenu.vue
+++ layouts/MobileIslandMenu.vue
@@ -5,9 +5,11 @@ import GridOnSharpIcon from "@/assets/icons/grid-on-sharp.svg?component";
 import SettingsIcon from "@/assets/icons/settings.svg?component";
 import AstronautHelmetIcon from "@/assets/icons/astronaut-helmet.svg?component";
 import { useLocalePath } from "#i18n";
+import { useI18n } from "vue-i18n";
+import MobileDropdownMenu from "@/layouts/MobileDropdownMenu.vue";
 
+const { t } = useI18n();
 const localePath = useLocalePath();
-
 const route = useRoute();
 </script>
 
@@ -18,69 +20,98 @@ const route = useRoute();
     <div class="flex justify-between w-full">
       <div class="flex items-center justify-around gap-3">
         <!-- Quests -->
-        <NuxtLink
-          :to="localePath('/user/quests')"
-          :class="[
-            ' flex-col items-center gap-0.5 block rounded-xl p-2 transition-colors ',
-            route.path === localePath('/user/quests')
-              ? 'text-white bg-blue-700'
-              : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
-          ]"
-        >
-          <GameControllerIcon class="size-7 stroke-[1.5]" />
-        </NuxtLink>
+        <div class="flex flex-col items-center">
+          <NuxtLink
+            :to="localePath('/user/quests')"
+            :class="[
+              ' flex-col items-center gap-0.5 block rounded-xl p-1.5 transition-colors ',
+              route.path === localePath('/user/quests')
+                ? 'text-white bg-blue-700'
+                : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
+            ]"
+          >
+            <GameControllerIcon class="size-6 stroke-[1.5]" />
+          </NuxtLink>
+          <span class="text-[10px] text-zinc-200">
+            {{ t("app.cabinet.nav.quests") }}
+          </span>
+        </div>
 
         <!-- Matrix -->
-        <NuxtLink
-          :to="localePath('/user/matrix')"
-          :class="[
-            ' flex-col items-center gap-0.1 block rounded-xl p-2 transition-colors flex-1',
-            route.path === localePath('/user/matrix')
-              ? 'text-white bg-blue-700'
-              : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
-          ]"
-        >
-          <GridOnSharpIcon class="size-7 stroke-[1.5]" />
-        </NuxtLink>
+        <div class="flex flex-col items-center">
+          <NuxtLink
+            :to="localePath('/user/matrix')"
+            :class="[
+              ' flex-col items-center gap-0.5 block rounded-xl p-1.5 transition-colors flex-1',
+              route.path === localePath('/user/matrix')
+                ? 'text-white bg-blue-700'
+                : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
+            ]"
+          >
+            <GridOnSharpIcon class="size-6 stroke-[1.5]" />
+          </NuxtLink>
+          <span class="text-[10px] text-zinc-200">
+            {{ t("app.cabinet.nav.matrices") }}
+          </span>
+        </div>
 
         <!-- Comments -->
-        <NuxtLink
-          :to="localePath('/user/daily-comment')"
-          :class="[
-            '  flex-col items-center gap-0.5 block rounded-xl p-2 transition-colors  justify-center',
-            route.path === localePath('/user/daily-comment')
-              ? 'text-white bg-blue-700'
-              : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
-          ]"
-        >
-          <CommentIcon class="size-7 stroke-[1.5]" />
-        </NuxtLink>
+        <div class="flex flex-col items-center">
+          <NuxtLink
+            :to="localePath('/user/daily-comment')"
+            :class="[
+              '  flex-col items-center gap-0.5 block rounded-xl p-1.5 transition-colors  justify-center',
+              route.path === localePath('/user/daily-comment')
+                ? 'text-white bg-blue-700'
+                : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
+            ]"
+          >
+            <CommentIcon class="size-6 stroke-[1.5]" />
+          </NuxtLink>
+          <span class="text-[10px] text-zinc-200">
+            {{ t("app.cabinet.nav.dailyComment") }}
+          </span>
+        </div>
       </div>
       <div class="flex items-center justify-around gap-3">
         <!-- Settings -->
-        <NuxtLink
-          :to="localePath('/user/settings')"
-          :class="[
-            ' flex-col items-center gap-0.5 block rounded-xl p-2 transition-colors',
-            route.path === localePath('/user/profile')
-              ? 'text-white bg-blue-700'
-              : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
-          ]"
-        >
-          <SettingsIcon class="size-7 stroke-[1.5]" />
-        </NuxtLink>
+        <!-- <div class="flex flex-col items-center">
+          <NuxtLink
+            :to="localePath('/user/settings')"
+            :class="[
+              ' flex-col items-center gap-0.5 block rounded-xl p-1.5 transition-colors',
+              route.path === localePath('/user/profile')
+                ? 'text-white bg-blue-700'
+                : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
+            ]"
+          >
+            <SettingsIcon class="size-6 stroke-[1.5]" />
+          </NuxtLink>
+          <span class="text-[10px] text-zinc-200">
+            {{ t("app.cabinet.nav.settings") }}
+          </span>
+        </div> -->
         <!-- Profile -->
-        <NuxtLink
-          :to="localePath('/user/profile')"
-          :class="[
-            ' flex-col items-center gap-0.5 block rounded-xl p-2 transition-colors',
-            route.path === localePath('/user/profile')
-              ? 'text-white bg-blue-700'
-              : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
-          ]"
-        >
-          <AstronautHelmetIcon class="size-7 stroke-[1.5]" />
-        </NuxtLink>
+        <!-- <div class="flex flex-col items-center">
+          <NuxtLink
+            :to="localePath('/user/profile')"
+            :class="[
+              ' flex-col items-center gap-0.5 block rounded-xl p-1.5 transition-colors',
+              route.path === localePath('/user/profile')
+                ? 'text-white bg-blue-700'
+                : 'text-zinc-300 hover:bg-blue-400 hover:text-white',
+            ]"
+          >
+            <AstronautHelmetIcon class="size-6 stroke-[1.5]" />
+          </NuxtLink>
+          <span class="text-[10px] text-zinc-200">
+            {{ t("app.cabinet.nav.profile") }}
+          </span>
+        </div> -->
+        <!-- Home -->
+        <div class="flex flex-col items-center">
+          <MobileDropdownMenu />
+        </div>
       </div>
     </div>
   </footer>
